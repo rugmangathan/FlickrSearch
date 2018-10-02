8d63d971ac06ee88acd86944ae04d99c92da4fe9
Rugmangathan <mkrugmangathan@gmail.com>
2018-10-02 19:04:16 +0530

    Added Canary tests.

---
+++ FlickrSearchTests/Canary.swift
@@ -0,0 +1,16 @@
+//
+//  Canary.swift
+//  FlickrSearchTests
+//
+//  Created by sherlock on 02/10/18.
+//  Copyright © 2018 rugmangathan. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrSearch
+
+class Canary: XCTestCase {
+  func testCanary() {
+    XCTAssertTrue(true)
+  }
+}
